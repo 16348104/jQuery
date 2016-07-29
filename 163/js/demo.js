c75b16ec6e00dde210217ba50a7605c54eeb9aaf
16348104 <16348104@qq.com>
2016-07-29 17:00:55 +0800

    163 study

--- 163/js/demo.js
+++ 163/js/demo.js
@@ -1,7 +1,17 @@
 /**
  * Created by zb on 2016/7/29.
  */
-$(function(){
+$(function () {
     //alert($(':radio').size());
-    alert($(':radio[name=sex]').eq(1).val());
+    //alert($(':radio[name=sex]').eq(1).val());
+    $('form').submit(function () {
+        alert('已提交！');
+    });
+    $('input').change(function () {
+        alert('文本改变！');
+    });
+    $('input').dblclick(function () {
+        alert('双击');
+
+    });
 });
